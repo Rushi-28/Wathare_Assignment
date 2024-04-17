abf3b995b067a551076afd65b8a80b4193f3c769
Rushi-28 <nikamrushikesh1111@gmail.com>
2024-04-17 15:13:58 +0530

    updated
    removed unwanted

--- Rest-Api using Express js/server2.js
+++ Rest-Api using Express js/server2.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,15 +8,13 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-// MongoDB connection settings
+
 const MONGO_URI = 'mongodb://127.0.0.1:27017';  
 const MONGO_DB_NAME = 'rushi';  
 const MONGO_COLLECTION_NAME = 'dbcollection';
 
-// Connect to MongoDB
+
 (async () => {
   let db;
   try {
@@ -36,13 +33,13 @@ const MONGO_COLLECTION_NAME = 'dbcollection';
   // Routes
   app.get('/data', async (req, res) => {
     try {
-      const collection = db.collection(MONGO_COLLECTION_NAME);
-console.log("Database:", db.databaseName);
-console.log("Collection:", MONGO_COLLECTION_NAME);
-console.log("Query:",collection.find({}).toArray());
-      const data = await collection.find({}).toArray(); // Properly fetching all documents
-		const count = await collection.countDocuments();
-console.log("Number of documents in collection:", count);
+        const collection = db.collection(MONGO_COLLECTION_NAME);
+	console.log("Database:", db.databaseName);
+	console.log("Collection:", MONGO_COLLECTION_NAME);
+	console.log("Query:",collection.find({}).toArray());
+        const data = await collection.find({}).toArray();
+	const count = await collection.countDocuments();
+	console.log("Number of documents in collection:", count);
 
       res.json(data);
     } catch (err) {
@@ -52,26 +49,6 @@ console.log("Number of documents in collection:", count);
 
 
 
-app.post('/add-data', async (req, res) => {
-  try {
-    const { documents } = req.body; // Assuming an array of documents is sent in the request body
-
-    if (!Array.isArray(documents)) {
-      return res.status(400).json({ message: 'Invalid data format. Expected an array of documents.' });
-    }
-
-    const collection = db.collection(MONGO_COLLECTION_NAME);
-    const result = await collection.insertMany(documents);
-
-    res.json(result);
-  } catch (err) {
-    res.status(500).json({ message: 'Failed to insert data', error: err.message });
-  }
-});
-
-
-
-  // Starting the server
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
